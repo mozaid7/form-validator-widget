cd2ded3c1f966884efea70b460c5be4bddea3312
Mohammed Zaid <mohammedzee98@gmail.com>
2026-02-12 16:24:14 +0530

    create: entry point

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export { FormValidator } from './components/FormValidator';
+export { FormField } from './components/FormField';
+
+export { useDebounce } from './hooks/useDebounce';
+
+export { validateField, validateForm } from './utils/validators';
+export { throttle } from './utils/polyfills';
+
+export type {
+  ValidationRule,
+  FormValidatorProps,
+  FormState,
+  ErrorState,
+  FormContextValue
+} from './types';
+
+import './utils/polyfills';
