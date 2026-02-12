f3d3dbdce8664752f4fe50c0a2e0e8b98b702a44
Mohammed Zaid <mohammedzee98@gmail.com>
2026-02-12 16:31:23 +0530

    configure: bundler rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+    input: 'src/index.ts',
+
+    output: [
+        {
+            file: 'dist/index.js',
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            file: 'dist/index.esm.js',
+            format: 'esm',
+            sourcemap: true,
+        }
+    ],
+
+    external: ['react', 'react-dom'],
+
+    plugins: [
+        resolve(),
+        commonjs(),
+        typescript({
+            tsconfig: './tsconfig.json',
+        }),
+    ],
+};
